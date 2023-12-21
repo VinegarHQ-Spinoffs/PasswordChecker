e558e2fe1120bcf6e4aeaec1206cabf8cda78fb8
Tracker-Friendly <arzumify@noreply.codeberg.org>
2023-12-21 14:33:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ pwd.addEventListener("input", function () {
   const pwdVal = pwd.value;
   let result = zxcvbn(pwdVal);  
   pwdStrength.className = "strength-" + result.score;  
-  pwdTime.textContent = "It would take " + result.crack_times_display.offline_fast_hashing_1e10_per_second + " to crack this password"
+  pwdTime.textContent = "It would take " + result.crack_times_display.online_no_throttling_10_per_second + " to crack this password"
 });
